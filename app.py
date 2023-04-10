768cc5c59358455e05dc993af2a3045cb67a418b
Will Araujo <114265989+williamvaraujo@users.noreply.github.com>
2023-04-10 12:25:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -104,6 +104,16 @@ def bot_das_pautas():
     print(ultima_mensagem)
     print(chat_id)
     print(nome_usuario)
+    
+    if not "text" in ultima_mensagem:
+        primeira_mensagem = request.get_json()
+        ultima_mensagem = primeira_mensagem["message"]["text"]
+        chat_id = primeira_mensagem["message"]["chat"]["id"]
+        nome_usuario = primeira_mensagem["message"]["from"]["first_name"]  
+        print(primeira_mensagem)
+        print(ultima_mensagem)
+        print(chat_id)
+        print(nome_usuario)
 #---------------------------------------------------------------------------- /START --> RESPOSTA1
     if ultima_mensagem == "/start":
         #MENSAGEM DE BOAS-VINDAS E ORIENTAÇÃO
