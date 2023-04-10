e6c048c389b193254134b86f7e832000035012d1
Will Araujo <114265989+williamvaraujo@users.noreply.github.com>
2023-04-10 04:14:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ cache = Cache(app, config={'CACHE_TYPE': 'null'})
 def bot_das_pautas():
     #['message']['chat']['id']['-1']['text']
     primeira_mensagem = request.json
-    ultima_mensagem = primeira_mensagem["message"]["chat"]["id"]
+    ultima_mensagem = primeira_mensagem["message"]["text"]
     chat_id = primeira_mensagem["message"]["chat"]["id"]
     nome_usuario = primeira_mensagem["message"]["from"]["first_name"]  
     print(primeira_mensagem)
